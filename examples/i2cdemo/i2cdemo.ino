82805702c671ccf418548e20574dd4defff41c36
ClosedCube <github@closedcube.com>
2020-06-03 21:26:48 +0000

    Updated

--- examples/i2cdemo/i2cdemo.ino
+++ examples/i2cdemo/i2cdemo.ino
@@ -3,7 +3,7 @@
 Example: i2cdemo
 
 Arduino library for ClosedCube I2C Driver (Wrapper)
-version 2020.5.29
+version 2020.6.3
 
 ---
 
--- src/ClosedCube_I2C.cpp
+++ src/ClosedCube_I2C.cpp
@@ -1,7 +1,7 @@
 /*
 
 Arduino library for ClosedCube I2C Driver (Wrapper)
-version 2020.5.29
+version 2020.6.3
 
 ---
 
